9d193cdfb8647e1d39dd57ec0dd35cde7146250a
Gulraiz Noor Bari ⚡ <78252747+gulraiznoorbari@users.noreply.github.com>
2024-01-19 03:23:40 +0500

    Add File

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static string grouping(int num)
+        {
+            string numStr = num.ToString();   // 21
+            ArrayList arrayList = new ArrayList();
+            string res = "";
+
+            for (int i = 1; i <= numStr.Length; i++)
+            {
+                if (i == numStr.Length || numStr[i] != numStr[i - 1])
+                {
+                    arrayList.Add(numStr[i - 1]);
+                    string temp = addDigitCount(arrayList);
+                    arrayList.Clear();
+                    res += temp;
+                }
+                else
+                {
+                    arrayList.Add(numStr[i - 1]);
+                }
+            }
+            return res;
+        }
+
+        private static string addDigitCount(ArrayList array)
+        {
+            StringBuilder res = new StringBuilder();
+            res.Append(array.Count);
+            res.Append(array[0]);
+            return res.ToString();
+        }
+
+        public static void Main(String[] args)
+        {
+            int number = 222;
+            String result = grouping(number);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
